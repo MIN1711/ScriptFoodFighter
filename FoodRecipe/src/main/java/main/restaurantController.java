5413e10ac2acb0db9620bd035132ec609042e206
MIN1711 <144243452+MIN1711@users.noreply.github.com>
2023-09-22 17:42:32 +0900

    restaurantController 설명 추가

--- FoodRecipe/src/main/java/main/restaurantController.java
+++ FoodRecipe/src/main/java/main/restaurantController.java
@@ -8,9 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// 맛집검색 화면으로 이동하는 controller
+// 목록과 위치정보, 지도의 출력은 모두 kakaoMap을 이용
 @WebServlet(value = "/restaurant.do")
 public class restaurantController extends HttpServlet {
-
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.getRequestDispatcher("restaurant.jsp").forward(req, resp);
